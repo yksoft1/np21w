504f12e778162894165d33ee239148cb29bcf128
yksoft1 <yksoft1@gmail.com>
2018-10-30 02:42:11 +0800

    SDL2 dosio Windows fix.

--- sdl2/dosio.c
+++ sdl2/dosio.c
@@ -373,7 +373,11 @@ const char	*ret;
 
 	ret = path;
 	while((csize = milstr_charsize(path)) != 0) {
+#if !defined(WIN32)
 		if ((csize == 1) && (*path == '/')) {
+#else
+		if ((csize == 1) && (*path == '\\')) {	
+#endif
 			ret = path + 1;
 		}
 		path += csize;
@@ -432,7 +436,11 @@ void file_cutseparator(char *path) {
 
 	pos = (int)strlen(path) - 1;
 	if ((pos > 0) &&							// 2文字以上でー
-		(path[pos] == '/') &&					// ケツが \ でー
+#if !defined(WIN32)
+ 		(path[pos] == '/') &&					// ケツが / でー
+#else
+ 		(path[pos] == '\\') &&					// ケツが \ でー
+#endif
 		((pos != 1) || (path[0] != '.'))) {		// './' ではなかったら
 		path[pos] = '\0';
 	}
@@ -443,9 +451,16 @@ void file_setseparator(char *path, int maxlen) {
 	int		pos;
 
 	pos = (int)strlen(path);
-	if ((pos) && (path[pos-1] != '/') && ((pos + 2) < maxlen)) {
-		path[pos++] = '/';
-		path[pos] = '\0';
-	}
+#if !defined(WIN32)
+ 	if ((pos) && (path[pos-1] != '/') && ((pos + 2) < maxlen)) {
+ 		path[pos++] = '/';
+ 		path[pos] = '\0';
+ 	}
+#else
+ 	if ((pos) && (path[pos-1] != '\\') && ((pos + 2) < maxlen)) {
+ 		path[pos++] = '\\';
+ 		path[pos] = '\0';
+ 	}	
+#endif
 }
 
